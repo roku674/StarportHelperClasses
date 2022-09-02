0754933364da710236ebcf751c16815ccd995d1b
roku674 <af03848@georgiasouthern.edu>
2022-09-02 17:47:35 -0400

    Create Helper.cs

---
+++ Helper.cs
@@ -0,0 +1,35 @@
+//Created by Alexander Fields https://github.com/roku674
+
+using System.Linq;
+
+namespace StarportHelperClasses
+{
+    public static class Helper
+    {
+        public static bool IsZoundsable(string planetType, string research)
+        {
+            string[] arch2Up = new string[] { "Arch lvl 2", "Arch lvl 3", "Arch lvl 4", "Arch lvl 5" };
+            string[] arch3Up = new string[] { "Arch lvl 3", "Arch lvl 4", "Arch lvl 5" };
+            string[] arch4Up = new string[] { "Arch lvl 4", "Arch lvl 5" };
+
+            if (planetType == "arctic" && arch2Up.Any(s => research.Contains(s)))
+            {
+                return true;
+            }
+            else if ((planetType == "rocky" || planetType == "greenhouse" || planetType == "Intergalactic paradise") && arch3Up.Any(s => research.Contains(s)))
+            {
+                return true;
+            }
+            else if ((planetType == "earthlike" || planetType == "volcanic" || planetType == "oceanic") && arch4Up.Any(s => research.Contains(s)))
+            {
+                return true;
+            }
+            else if ((planetType == "mountainous" || planetType == "desert") && research.Contains("Arch lvl 5"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
